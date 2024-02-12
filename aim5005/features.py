2d1de2fb80caa223c62b103ad3309e7d68e716c8
offbeatraj <rbehera@mail.yu.edu>
2024-02-12 14:29:34 -0500

    machine learning assignment solved

--- aim5005/features.py
+++ aim5005/features.py
@@ -31,7 +31,7 @@ class MinMaxScaler:
         diff_max_min = self.maximum - self.minimum
         
         # TODO: There is a bug here... Look carefully! 
-        return x-self.minimum/(self.maximum-self.minimum)
+        return (x-self.minimum)/(self.maximum-self.minimum)
     
     def fit_transform(self, x:list) -> np.ndarray:
         x = self._check_is_array(x)
@@ -40,6 +40,37 @@ class MinMaxScaler:
     
     
 class StandardScaler:
-    def __init__(self):
+    def _init_(self):
         self.mean = None
-        raise NotImplementedError
+        self.std = None
+        
+    def _check_is_array(self, x:np.ndarray) -> np.ndarray:
+        """
+        Try to convert x to a np.ndarray if it'a not a np.ndarray and return. If it can't be cast raise an error
+        """
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        return x
+        
+    
+    def fit(self, x:np.ndarray) -> None:   
+        x = self._check_is_array(x)
+        self.mean=x.mean(axis=0)
+        self.std=x.std(axis=0)
+        
+    def transform(self, x:np.ndarray) -> list:
+        """
+        MinMax Scale the given vector
+        """
+        x = self._check_is_array(x)
+
+        # TODO: There is a bug here... Look carefully! 
+        return (x-self.mean)/self.std
+    
+    def fit_transform(self, x:list) -> np.ndarray:
+        x = self._check_is_array(x)
+        self.fit(x)
+        return self.transform(x)
+        
